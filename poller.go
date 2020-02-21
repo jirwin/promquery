2f44456a8a8ea4bf5f0ba2d93c8ab7302ba4ba14
Steven Elleman <steven.elleman@okta.com>
2020-02-20 18:11:27 -0800

    Add deadline to wait method

--- poller.go
+++ poller.go
@@ -175,7 +175,7 @@ func (p *Poller) Init(ctx context.Context) {
 	wg.Wait()
 }
 
-func (p *Poller) Wait(ctx context.Context, interval time.Duration) <-chan error {
+func (p *Poller) Wait(ctx context.Context, interval time.Duration, deadline time.Duration) <-chan error {
 	doneChan := make(chan error)
 	go func() {
 		p.timer = monotime.New()
@@ -238,6 +238,11 @@ func (p *Poller) Wait(ctx context.Context, interval time.Duration) <-chan error 
 						successful := 0
 
 						for {
+							if p.timer.Elapsed() > deadline {
+								waitErr <- fmt.Errorf("error: elapsed time exceeded deadline")
+								return
+							}
+
 							fmt.Printf("Waiting %s (%s elapsed) to poll for metrics\n", interval, p.timer.Elapsed())
 							<-time.After(interval)
 
